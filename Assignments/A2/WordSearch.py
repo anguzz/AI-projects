64876da59414f2082819a7fe9713f9d628f9589a
Angel S <santoyox714@gmail.com>
2023-02-09 18:08:19 -0800

    Update WordSearch.py

--- Assignments/A2/WordSearch.py
+++ Assignments/A2/WordSearch.py
@@ -1,17 +1,10 @@
-# Below lists detail all eight possible movements from a cell
 row = [-1, -1, -1, 0, 0, 1, 1, 1]
 col = [-1, 0, 1, -1, 1, -1, 0, 1]
 
-
-# Function to check if it is possible to go to position next
-# from the current position. The function returns false if next is
-# not in a valid position, or it is already visited
 def isValid(mat, x, y, path):
 	return (0 <= x < len(mat)) and (0 <= y < len(mat[0])) and (x, y) not in path
 
-
 def DFS(mat, word, i, j, path=[], index=0):
-    # return if characters don't match
     if mat[i][j] != word[index]:
         return None
     path.append((i, j))
@@ -22,8 +15,6 @@ def DFS(mat, word, i, j, path=[], index=0):
             if isValid(mat, i + row[z], j + col[z], path):
                 DFS(mat, word, i + row[z], j + col[z], path, index + 1)
     path.pop()
-
-
 
 def WordSearch(mat, word):
 	# base case
